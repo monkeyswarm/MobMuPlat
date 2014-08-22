30e57681f899afa82917842aa15b2d7894c73498
diglesia <diglesia@google.com>
2014-08-22 12:09:51 -0700

    update version to 1.60, add display of IP address, edit patch files

--- MobMuPlat/gui/MeTable.h
+++ MobMuPlat/gui/MeTable.h
@@ -2,7 +2,7 @@
 //  MeTable.h
 //  MobMuPlat
 //
-//  Created by diglesia on 4/24/14.
+//  Created by Daniel Iglesia on 4/24/14.
 //  Copyright (c) 2014 Daniel Iglesia. All rights reserved.
 //
 
--- MobMuPlat/gui/MenuViewController.h
+++ MobMuPlat/gui/MenuViewController.h
@@ -2,7 +2,7 @@
 //  MenuViewController.h
 //  MobMuPlat
 //
-//  Created by diglesia on 4/9/14.
+//  Created by Daniel Iglesia on 4/9/14.
 //  Copyright (c) 2014 Daniel Iglesia. All rights reserved.
 //
 
